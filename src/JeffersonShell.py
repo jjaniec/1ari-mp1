ab14b6b479e1be2af58031fbd1796d5eea00175b
jjaniec <jjaniec@firemail.cc>
2017-02-03 21:59:25 +0100

    add convertLetters function

---
+++ src/JeffersonShell.py
@@ -0,0 +1,11 @@
+import  sys     #For debug purposes
+
+def     convertLetter(text):
+    tmp_str = ""
+
+    for letter in text:
+        if letter.isalpha() and ord(letter) <= 123:
+            tmp_str += letter
+    return (tmp_str)
+
+
