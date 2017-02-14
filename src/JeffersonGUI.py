baa8b2d25e8f019b800e15383882b5f968f70d99
jjaniec <jjaniec@firemail.cc>
2017-02-14 20:40:02 +0100

    add display cylinderS function

--- src/JeffersonGUI.py
+++ src/JeffersonGUI.py
@@ -3,15 +3,18 @@ from pygame.locals import *
 
 pygame.init()
 
-def displayCylinder(mySurface,cylinder,i):
+def displayCylinder(mySurface, cylinder, i):
     RED = (255,0,0)
-    fontObj = pygame.font.Font('Roboto-Light.ttf',16)
+    fontObj = pygame.font.Font('Roboto-Light.ttf', 16)
     for n in range(len(cylinder.get(i))):
-        #print(cylinder.get(i)[n])
         texteSurface = fontObj.render(cylinder.get(i)[n], True, RED)
         texteRect = texteSurface.get_rect()
-        texteRect.topleft = (30+20*i, 20+20*n)
+        texteRect.topleft = (30 + 20 * i, 20 + 20 *n)
         mySurface.blit(texteSurface, texteRect)
 
+def displayCylinders(mySurface, cylinder):
+    for i in range(1, len(cylinder) + 1):
+        displayCylinder(mySurface, cylinder, i)
+        pygame.display.update()
 
 jeffersonGUI()
