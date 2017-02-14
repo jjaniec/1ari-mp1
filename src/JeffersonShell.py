018dbe2bfa50b8846dafab4102392323ce6e5a44
jjaniec <jjaniec@firemail.cc>
2017-02-14 20:34:51 +0100

    faster mix function

--- src/JeffersonShell.py
+++ src/JeffersonShell.py
@@ -1,11 +1,11 @@
 import  sys     #For debug purposes
 import  random
-from    collections import Counter
 
-FILEPATH    = "../MP-1ARI.txt"
-#FILENL      = 15 #createCylinder(->n)
+FILEPATH    = "../CreatedFile.txt"
+FILENL      = 20 #createCylinder(->n)
 
-def     convertLetter(text):
+
+def convertLetter(text):
     tmp_str = ""
 
     for letter in text:
@@ -13,23 +13,23 @@ def     convertLetter(text):
             tmp_str += letter
     return (tmp_str.upper())
 
-def     mix():
-    buf_str = ""
+def mix():
+    letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    chain = ''
+    for i in range(len(letters)):
+        s = random.randint(0,len(letters)-1)
+        chain += letters[s]
+        letters.remove(letters[s])
+    return chain
 
-    while (len(buf_str) < 26):
-        buf_char = chr(random.randint(0, 25) + 65)
-        if buf_char not in buf_str:
-            buf_str += buf_char
-    return (buf_str)
-
-def     createCylinder(file, n):
+def createCylinder(file, n):
     file_ = open(file, "w")
 
     for i in range(0, n):
         file_.write(mix() + "\n")
     file_.close()
 
-def     ft_getnewlinesnb(str_):
+def ft_getnewlinesnb(str_):
     count = 0
 
     for i in range(0, len(str_)):
@@ -37,7 +37,7 @@ def     ft_getnewlinesnb(str_):
             count += 1
     return (count)
 
-def     loadCylinder(file):
+def loadCylinder(file):
     content = open(file, "r").read()
     lines_dict = {}
     i = 0
@@ -48,42 +48,55 @@ def     loadCylinder(file):
             buf += content[(i * 27) + j]
         lines_dict[i + 1] = buf
         buf = ""
+
     return (lines_dict)
 
-def     keyOK(key ,n):
-    for i in range(1, n+1):
+def keyOk(key, n):
+    for i in range(1,n+1):
         if i not in key:
-            return (False)
-    return (True)
+            return False
+    return True
 
-def     createKey(n):
-    tmp_str = ""
-    while (n != 0):
-        tmp_str += str(n)
-        n -= 1
-    return (''.join(random.sample(tmp_str,len(tmp_str))))
 
-def     find(letter, alphabet):
+def createKey(n):
+    lst = []
+    key = []
+    for num in range(1,n+1):
+        lst += [num]
+    for i in range(n):
+        k = random.randint(0,len(lst)-1)
+        key += [lst[k]]
+        lst.remove(lst[k])
+    return key
+
+def find(letter, alphabet):
     for i in range(len(alphabet)):
         if letter == alphabet[i]:
-            return (i)
+            return i
 
-def     shift(i):
-    return ((-1), ((i + 6) % 26))[(i <= 25 and i >= 0)]
+def shift(i):
+    i += 6
+    i %= 26
+    return i
 
-def     unshift(i):
-    return ((-1), ((i - 6) % 26))[(i <= 25 and i >= 0)]
+def unshift(i):
+    i -= 6
+    i %= 26
+    return i
 
-def     cipherLetter(letter, alphabet):
-    return (shift(find(letter, alphabet)))
+def cipherLetter(letter, alphabet):
+    i = find(letter,alphabet)
+    i = shift(i)
+    return i
 
-def     uncipherLetter(letter, alphabet):
-    return (unshift(find(letter, alphabet)))
+def uncipherLetter(letter, alphabet):
+    i = find(letter, alphabet)
+    i = unshift(i)
+    return i
 
-def cipherText(cylinder,key,text):
+def cipherText(cylinder, key, text):
     k = 0
     c = ''
-
     if len(cylinder) != len(key):
         return('The key is invalid')
     text = convertLetter(text)
@@ -93,10 +106,9 @@ def cipherText(cylinder,key,text):
         k += 1
     return c
 
-def uncipherText(cylinder,key,text):
+def uncipherText(cylinder, key, text):
     k = 0
     c = ''
-
     if len(cylinder) != len(key):
         return ('The key is invalid')
     text = convertLetter(text)
@@ -106,8 +118,13 @@ def uncipherText(cylinder,key,text):
         k += 1
     return c
 
-#cylinder = loadCylinder(FILEPATH)
-#h = 'supinfo'
-#key = [7, 4, 19, 13, 17, 18, 12, 15, 16, 10, 11, 2, 5, 20, 9, 8, 1, 14, 6, 3]
+cylinder = loadCylinder(FILEPATH)
+h = 'XHWNIVTGBMZREQMFIREA'
+key = [7, 4, 19, 13, 17, 18, 12, 15, 16, 10, 11, 2, 5, 20, 9, 8, 1, 14, 6, 3]
 
-#print(cipherText(cylinder,key,h))
+#print(uncipherText(cylinder,key,h))
+print(cylinder)
+
+
+#createCylinder(FILEPATH, FILENL)
+#print(createKey(FILENL))
